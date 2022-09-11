b55f02f3560cb8cdd5934c9704fe0080acbc9201
To-Oy <31363408+To-Oy@users.noreply.github.com>
2022-09-11 18:07:26 +0900

    Update encoding-japanese.js

--- functions/encoding-japanese.js
+++ functions/encoding-japanese.js
@@ -1,6 +1,32 @@
-exports.handler = async () => {
+import encoding from "encoding-japanese";
+import fetch from "node-fetch";
+
+exports.handler = async function (event, context, callback) {
+  const queryParameters = event.queryStringParameters
+  if (!queryParameters.url) {
+    return {
+      statusCode: 400,
+      body: "query patameters `url` must be specified."
+    }
+  }
+  const response = await fetch(queryParameters.url)
+  const binary = await response.buffer()
+
+  const utf8text = Buffer.from(encoding.convert(binary, "UTF8")).toString("utf-8")
+
+  var replaced;
+  switch (encoding.detect(binary)) {
+    case "SJIS":
+      replaced = utf8text
+        .replace("charset=Shift_JIS", "charset=UTF-8")
+        .replace('<?xml version="1.0" encoding="shift_jis"?>', '');
+      break;
+    default:
+      break;
+  }
+
   return {
     statusCode: 200,
-    body: 'Hello World',
+    body: replaced
   };
-};
+}
